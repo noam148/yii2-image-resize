0f7a75cc35c08f26b18201ae57234f4bb3b353b3
noam148 <noam@24nieuwe.nl>
2016-11-23 22:28:12 +0100

    Upload project

---
+++ ImageResize.php
@@ -0,0 +1,133 @@
+<?php
+namespace noam148\imageresize;
+
+use Yii;
+use yii\helpers\Url;
+use yii\helpers\FileHelper;
+use yii\imagine\Image;
+use Imagine\Image\Box;
+use Imagine\Image\ManipulatorInterface;
+
+class ImageResize
+{
+    const IMAGE_OUTBOUND = ManipulatorInterface::THUMBNAIL_OUTBOUND;
+    const IMAGE_INSET = ManipulatorInterface::THUMBNAIL_INSET;
+
+	/** @var string $cachePath path alias relative with webroot where the cache files are kept */
+    public $cachePath = 'assets/images';
+    /** @var int $cacheExpire */
+    public $cacheExpire = 0;
+	public $imageQuality = 50;
+	public $useFilename = true;
+	public $absoluteUrl = false;
+
+    /**
+     * Creates and caches the image thumbnail and returns full path from thumbnail file.
+     *
+     * @param string $filePath to original file
+     * @param integer $width
+     * @param integer $height
+     * @param string $mode
+	 * @param integer $quality (1 - 100 quality)
+	 * @param string $chosenFileName (custome filename)
+     * @return string
+     * @throws FileNotFoundException
+     */
+    public function generateImage($filePath, $width, $height, $mode = self::IMAGE_OUTBOUND, $quality = null, $chosenFileName = null)
+    {
+		$filePath = FileHelper::normalizePath(Yii::getAlias($filePath));
+        if (!is_file($filePath)) {
+            throw new FileNotFoundException("File $filePath doesn't exist");
+        }
+		//create some vars
+		$cachePath = Yii::getAlias('@webroot/' . $this->cachePath);
+		//get fileinfo
+		$aFileInfo = pathinfo($filePath);
+		//set default filename
+		$sFileHash = md5($filePath . $width . $height . $mode . filemtime($filePath));
+		$imageFileName = null;
+		//if $this->useFilename set to true? use seo friendly name
+		if($this->useFilename === true){
+			//set hash and default name
+			$sFileHashShort = substr($sFileHash, 0, 6);
+			$sFileName = $aFileInfo['filename'];
+			//set choosen filename if $chosenFileName not null.
+			if($chosenFileName !== null){
+				$sFileName = preg_replace('/(\.\w+)$/', '', $chosenFileName);
+			}
+			//replace for seo friendly file name
+			$sFilenameReplace = preg_replace("/[^\w\.\-]+/", '-', $sFileName);
+			//set filename
+			$imageFileName = $sFileHashShort."_".$sFilenameReplace;
+		//else use file hash as filename	
+		}else{
+			$imageFileName = $sFileHash;
+		}
+	
+	    $imageFileExt = ".".$aFileInfo['extension'];
+		$imageFilePath = $cachePath . DIRECTORY_SEPARATOR . substr($imageFileName, 0, 2);
+        $imageFile = $imageFilePath . DIRECTORY_SEPARATOR . $imageFileName . $imageFileExt;
+		
+        if (file_exists($imageFile)) {
+            if ($this->cacheExpire !== 0 && (time() - filemtime($imageFile)) > $this->cacheExpire) {
+                unlink($imageFile);
+            } else {
+                return $imageFile;
+            }
+        }
+		//if dir not exist create cache edir
+        if (!is_dir($imageFilePath)) {
+			FileHelper::createDirectory($imageFilePath, 0755);
+        }
+		//create image
+        $box = new Box($width, $height);
+        $image = Image::getImagine()->open($filePath);
+        $image = $image->thumbnail($box, $mode);
+        
+        $options = [
+            'quality'=> $quality === null ? $this->imageQuality : $quality
+        ]; 
+        $image->save($imageFile, $options);
+        return $imageFile;
+    }
+    /**
+     * Creates and caches the image and returns URL from resized file.
+     *
+     * @param string $filePath to original file
+     * @param integer $width
+     * @param integer $height
+     * @param string $mode
+	 * @param integer $quality (1 - 100 quality)
+	 * @param string $fileName (custome filename)
+     * @return string
+     */
+    public function getUrl($filePath, $width, $height, $mode = self::THUMBNAIL_OUTBOUND, $quality = null, $fileName = null)
+    {	
+		//get original file 
+		$normalizePath = FileHelper::normalizePath(Yii::getAlias($filePath));
+		//get cache url
+        $cacheUrl = Yii::getAlias($this->cachePath);
+		//generate file
+        $resizedFilePath = self::generateImage($normalizePath, $width, $height, $mode, $quality, $fileName);
+		//get resized file
+		$normalizeResizedFilePath = FileHelper::normalizePath($resizedFilePath);
+		$resizedFileName = pathinfo($normalizeResizedFilePath,PATHINFO_BASENAME);
+		//get url
+		$sFileUrl = Url::to('@web/'.$cacheUrl . '/' . substr($resizedFileName, 0, 2) . '/' . $resizedFileName, $this->absoluteUrl);
+		//return path
+        return $sFileUrl; 
+    }
+    /**
+     * Clear cache directory.
+     *
+     * @return bool
+     */
+    public function clearCache()
+    {
+        $cachePath = Yii::getAlias('@webroot/' . $this->cachePath);
+		//remove dir
+        FileHelper::removeDirectory($cachePath);
+		//creat dir
+        return FileHelper::createDirectory($cachePath, 0755);
+    }
+}
---
+++ README.md
@@ -0,0 +1,55 @@
+Image resize for Yii2
+========================
+
+A Yii2 component for resizing images and store it in a cache folder
+
+Installation
+------------
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+* Either run
+
+```
+php composer.phar require "noam148/yii2-image-resize" "dev-master"
+```
+or add
+
+```json
+"noam148/yii2-image-resize" : "dev-master"
+```
+
+to the require section of your application's `composer.json` file.
+
+* Add a new component in `components` section of your application's configuration file, for example:
+
+```php
+'components' => [
+    'imageresize' => [
+		'class' => 'noam148\imageresize\ImageResize',
+		//path relative web folder
+		'cachePath' => 'assets/images',
+		//use filename (seo friendly) for resized images else use a hash
+		'useFilename' => true,
+		//show full url (for example in case of a API)
+		'absoluteUrl' => false,
+	],
+],
+```
+
+Usage
+-----
+
+If you want to get a image url:
+
+```php
+/*
+ * $sImageFilePath_id: (required) path to file
+ * $width/$height: (required) width height of the image
+ * $mode: ImageResize::IMAGE_OUTBOUND or ImageResize::IMAGE_INSET
+ * $$quality: (1 - 100)
+ * $chosenFileName: if config -> components -> imageresize -> useFilename is true? its an option to give a custom name else use original file name
+ */
+\Yii::$app->imageresize->getUrl($sImageFilePath, $width, $height, $mode, $quality, $chosenFileName);
+```
+
+**If you got questions, tips or feedback? Please, let me know!**
